5959b4fc84a1e6bedd5f5d9585af31a5e53f18e1
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 10:45:41 -0500

    fix: alquileres_controllers

---
+++ src/alquileres/schema/alquileres.controllers.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AlquileresService } from './alquileres.service';
+import { AlquileresDTO } from './dto/alquileres.dto';
+
+@Controller('api/v2/autores')
+export class AlquileresController {
+    constructor(private alquileresService:AlquileresService){}
+
+    @Post()
+    insertar(@Body() alquileresDTO:AlquileresDTO){
+        return this.alquileresService.insertar(alquileresDTO);
+    }
+
+    @Get()
+    todos(){
+        return this.alquileresService.todos();
+    }
+
+    @Get(':id')
+    uno(@Param('id') id:string){
+        return this.alquileresService.uno(id);
+    }
+
+    @Put(':id')
+    actualizar(@Param('id') id:string, @Body() alquileresDTO:AlquileresDTO){
+        return this.alquileresService.actualizar(id, alquileresDTO);
+    }
+
+    @Delete(':id')
+    eliminar(@Param('id') id:string){
+        return this.alquileresService.eliminar(id);
+    }
+}
