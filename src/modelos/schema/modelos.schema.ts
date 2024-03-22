9ab43c092a4125a24710f048ee0b1c0948699479
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 12:33:25 -0500

    fix: modelos_schema

---
+++ src/modelos/schema/modelos.schema.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const ModelosSchema = new mongoose.Schema({
+    nombre:{type:Date, required:true},
+    marca:[{type:mongoose.Schema.Types.ObjectId, ref:'modelos'}],
+},{
+    timestamps:true,
+});
