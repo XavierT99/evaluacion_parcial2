f7f6af75377f781ab23ec3abc1224b797db68f86
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 11:27:30 -0500

    fix:clientes_dto

--- src/clientes/dto/clientes.dto.ts
+++ src/clientes/dto/clientes.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class ClientesDTO {
-    @IsNotEmpty({message: 'El campo del título no puede estar vacío'})
-    @IsString({message: 'El campo titulo debe ser de tipo string'})
+    @IsNotEmpty({message: 'El nombre es requerido'})
+    @IsString({message: 'El nombre debe ser un string'})
     readonly titulo: string;
 }
