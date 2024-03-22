3be4166018744fe12292adcbc9f98d2b0f371958
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 12:59:38 -0500

    fix: app_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AlquileresModule } from './alquileres/alquileres.module';
+import { ClientesModule } from './clientes/clientes.module';
+import { CochesModule } from './coches/coches.module';
+import { ModelosModule } from './modelos/modelos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.urimongo),
+    AlquileresModule,
+    ClientesModule,
+    CochesModule,
+    ModelosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
