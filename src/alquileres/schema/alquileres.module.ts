84dd53f07013e7a1673572f989f08601f65b1803
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 10:49:22 -0500

    fix:alquileres_module

---
+++ src/alquileres/schema/alquileres.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AlquileresController } from './alquileres.controller';
+import { AlquileresService } from './alquileres.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ALQUILERES} from 'src/models/models';
+import { AlquileresSchema } from './schema/alquileres.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeatureAsync([{
+      name: ALQUILERES.name,
+      useFactory: () => {
+        return AlquileresSchema;
+      },
+    },
+  ]),
+  ],
+  controllers: [AlquileresController],
+  providers: [AlquileresService],
+  exports: [AlquileresService],
+})
+export class AutoresModule {}
