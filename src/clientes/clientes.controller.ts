6550858b1f40c898102dc16d3ded1fb16d364b0f
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 19:49:08 -0500

    fix:clientes_controller

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -1,33 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClientesService } from './clientes.service';
 import { ClientesDTO } from './dto/clientes.dto';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('Clientes')
 @Controller('api/v2/clientes')
 export class ClientesController {
-    constructor(private readonly clientesService: ClientesService){}
+  constructor(private readonly clientesService: ClientesService) {}
 
-    @Post()
-    insertar(@Body() clientesDTO:ClientesDTO){
-        return this.clientesService.insertar(clientesDTO);
-    }
+  @Post()
+  insertar(@Body() clientesDTO: ClientesDTO) {
+    return this.clientesService.insertar(clientesDTO);
+  }
 
-    @Get()
-    todos(){
-        return this.clientesService.todos();
-    }
+  @Get()
+  todos() {
+    return this.clientesService.todos();
+  }
 
-    @Get(':id')
-    uno(@Param('id') id:string){
-        return this.clientesService.uno(id);
-    }
+  @Get(':id')
+  uno(@Param('id') id: string) {
+    return this.clientesService.uno(id);
+  }
 
-    @Put(':id')
-    actualizar(@Param('id') id:string, @Body() clientesDTO:ClientesDTO){
-        return this.clientesService.actualizar(id, clientesDTO);
-    }
+  @Put(':id')
+  actualizar(@Param('id') id: string, @Body() clientesDTO: ClientesDTO) {
+    return this.clientesService.actualizar(id, clientesDTO);
+  }
 
-    @Delete(':id')
-    eliminar(@Param('id') id:string){
-        return this.clientesService.eliminar(id);
-    }
+  @Delete(':id')
+  eliminar(@Param('id') id: string) {
+    return this.clientesService.eliminar(id);
+  }
 }
--- src/clientes/schema/clientes.schema.ts
+++ src/clientes/schema/clientes.schema.ts
@@ -1,9 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export const ClientesSchema = new mongoose.Schema({
-    nombre: {type: String, required: true},
-    cedula: [{type: mongoose.Schema.Types.ObjectId, ref: 'cedula'}],
-    edad: {type: mongoose.Schema.Types.ObjectId, ref: 'edad'}
-},{
-    timestamps: true
-});
+export const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    clientesname: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+ClientesSchema.index({ email: 1 }, { unique: true });
+ClientesSchema.index({ clientesname: 1 }, { unique: true });
