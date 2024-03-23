8a3a277ea10fed9e2be79ca70d2432bbfe76dc28
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 19:18:06 -0500

    fix: http

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,20 +1,30 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
-    catch(exception: any, host: ArgumentsHost) {
-        const contexto = host.switchToHttp();
-        const response = contexto.getResponse();
-        const request = contexto.getRequest();
+  catch(exception: any, host: ArgumentsHost) {
+    const contexto = host.switchToHttp();
+    const response = contexto.getResponse();
+    const request = contexto.getRequest();
 
-        const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
 
-        const message = exception instanceof HttpException ? exception.getResponse() : exception;
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
 
-        response.status(status).json({
-            time: new Date().toISOString(),
-            path: request.url,
-            error: message
-        })
-    }
+    response.status(status).json({
+      time: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
 }
