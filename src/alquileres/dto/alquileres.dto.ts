ded3e2beb54d8345615fff9886dcaf28c79d1162
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 19:28:11 -0500

    fix:alquileres_dto

--- src/alquileres/dto/alquileres.dto.ts
+++ src/alquileres/dto/alquileres.dto.ts
@@ -1,7 +1,13 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class AlquileresDTO{
-    @IsNotEmpty({message: 'El nombre es requerido'})
-    @IsString({message: 'El nombre debe ser un texto'})
-    readonly nombre: string;
+export class AlquileresDTO {
+  @ApiProperty()
+  @IsNotEmpty({ message: 'El campo es obligatorio' })
+  @IsString()
+  readonly name: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'El campo es obligatorio' })
+  @IsString()
+  readonly email: string;
 }
