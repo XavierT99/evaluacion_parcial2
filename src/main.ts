91cf90fe230dbab15fb3290f732854d48e0140b4
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 19:23:09 -0500

    fix:main_ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeoutInterceptor());
-  await app.listen(3000);
+  app.enableCors();
+  const opciones = new DocumentBuilder()
+    .setTitle('Evaluacion Parcial2')
+    .setDescription('Documentacion de Evaluacion Parcial2')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, opciones);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
+
 
