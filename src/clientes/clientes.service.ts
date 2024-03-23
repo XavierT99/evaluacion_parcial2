d8abe3eb33d3d995426cc0814a324837978055b5
XavierTorres <xavier-torres99@hotmail.com>
2024-03-22 20:02:47 -0500

    fix:clientes_service

--- src/clientes/clientes.service.ts
+++ src/clientes/clientes.service.ts
@@ -3,31 +3,40 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CLIENTES } from 'src/models/models';
 import { IClientes } from './clientes.interfaces';
+import * as bcrypt from 'bcrypt';
 import { ClientesDTO } from './dto/clientes.dto';
-
 @Injectable()
 export class ClientesService {
-    constructor(@InjectModel (CLIENTES.name) private readonly model:Model<IClientes>){}
+  constructor(@InjectModel(CLIENTES.name) private readonly modelo: Model<IClientes>) {}
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+  async insertar(clientesDTO: ClientesDTO): Promise<IClientes> {
+    const hash = await this.hashPassword(clientesDTO.password);
+    const newUser = new this.modelo({ ...clientesDTO, password: hash });
+    return await newUser.save();
+  }
+  async todos(): Promise<IClientes[]> {
+    return await this.modelo.find();
+  }
+  async uno(id: string): Promise<IClientes> {
+    return await this.modelo.findById(id);
+  }
+  async actualizar(id: string, userDTO: ClientesDTO): Promise<IClientes> {
+    const hash = await this.hashPassword(clientesDTO.password);
+    const clientes = { ...clientesDTO, password: hash };
+    return await this.modelo.findByIdAndUpdate(id, clientes, { new: true });
+  }
+  async eliminar(id: string) {
+    await this.modelo.findByIdAndDelete(id);
+    return { status: HttpStatus.OK, msg: 'Cliente eliminado' };
+  }
 
-    async insertar (clientesDTO: ClientesDTO):Promise<IClientes>{
-        const nuevoClientes = new this.model(clientesDTO);
-        return await nuevoClientes.save();
-    }
-
-    async todos():Promise<IClientes[]>{
-        return await this.model.find();
-    }
-
-    async uno (id:string):Promise<IClientes>{
-        return await this.model.findById(id);
-    }
-
-    async actualizar (id:string, clientesDTO:ClientesDTO):Promise<IClientes>{
-        return await this.model.findByIdAndUpdate(id, clientesDTO, {new:true});
-    }
-
-    async eliminar (id:string){
-        await this.model.findByIdAndDelete(id);
-        return {status: HttpStatus.OK, message: 'Cliente eliminado'};
-    }
+  async BuscarporNombre(clientesname: string) {
+    return await this.modelo.findOne({ clientesname: clientesname });
+  }
+  async verficaContrasenia(password: string, passwordDB: string) {
+    return await bcrypt.compare(password, passwordDB);
+  }
 }
